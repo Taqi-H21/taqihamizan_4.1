2806e4ac5b1de0a1286a896a51f7421f6e5ceba8
Taqi Hamizan <taqihamizan21@gmail.com>
2024-09-21 18:19:43 +0700

    Add files via upload

---
+++ C++1_2410631170113_Taqi Hamizan_4.1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, i;
+
+    cout << "input: ";
+    cin >> n;
+
+        for (int i = 1; i <= n; i++)
+        {
+            {
+                            cout << " + " << i;
+            }
+        }
+            cout << " = " << n / 2 *(2 + (n - 1));
+return 0;
+}
